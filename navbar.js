41d1748aa61d845affead7b80ed5338cb4458ae9
Tahir Saeed <tahirsaeedt7009@gmail.com>
2022-04-14 17:32:16 +0500

    Live

--- navbar.js
+++ navbar.js
@@ -8,3 +8,19 @@ if (x.style.display === "none") {
     x.style.display = "none";
   }
 }
+// best();
+// function best(){
+// 	console.log("cange");
+// var w=screen.width;
+// if(w>600)
+// {
+// 	var a = document.getElementById("hamburger-icon");
+//  a.style.visibility = "hidden";
+
+// }
+// else{
+//  a.style.display = "visible";
+
+// }
+// }
+// window.addEventListener('resize', best);
